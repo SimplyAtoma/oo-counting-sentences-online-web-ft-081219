55d266770a967a6c4bede7f841618852eb0dec5c
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-26 20:27:02 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+     split_sentence = self.split( "." && "!" && "?")
+     split_sentence.each do |phrase|
+       if phrase.sentence? 
+         count += 1 
+       end
+       if phrase.question?
+         count += 1 
+       end
+       if phrase.exclamation?
+         count += 1 
+       end
+       puts phrase"\n"
+       binding.pry
+     end
+     count
   end
 end
